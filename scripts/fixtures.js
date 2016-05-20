1ffd758f84f1209a94f14def0154b04b31a5ea49
Jordan Epps <jordan.k.epps@gmail.com>
2016-05-20 16:15:35 -0700

    score is displayed and updates when appropriate

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -3,13 +3,15 @@ var context = canvas.getContext('2d');
 var player1 = new Player();
 var computer = new Computer();
 var ball = new Ball(395, 245, 10, 10);
+var $player1Score = document.getElementsByClassName('player-score')[0];
+var $computerScore = document.getElementsByClassName('computer-score')[0];
 
 function Ball(x, y, width, height) {
 	this.x = x;
 	this.y = y;
 	this.width = width;
 	this.height = height;
-	this.speed = 12;	
+	this.speed = 10;	
 	if(Math.random()>=0.5) {
 		this.angle = Math.PI + (-1 + 2 * Math.random()) * Math.PI / 3;
 	} else {
@@ -46,16 +48,20 @@ function Ball(x, y, width, height) {
 		context.clearRect(this.x,this.y,this.width,this.height)
 		if(this.y <= 5 || this.y >= canvas.height - this.height/2) {
 			this.ySpeed *= -1;
-		}
+		}	
 		if(this.x <= player1.paddle.x + player1.paddle.width && this.x >= player1.paddle.x && this.y >= player1.paddle.y && this.y <= player1.paddle.y + player1.paddle.height ||
 			 this.x <= computer.paddle.x && this.x >= computer.paddle.x - computer.paddle.width && this.y >= computer.paddle.y && this.y <= computer.paddle.y + computer.paddle.height) {
 			this.xSpeed *= -1;
+			this.ySpeed *= (Math.random() * 3 + 1)/2;
+			if(Math.random() > 0.5) {this.ySpeed *= -1;}
 		}
 		if (this.x + this.width > canvas.width) {
 			computer.score++;
+			$computerScore.innerHTML = computer.score;
 			this.reset();
 		} else if (this.x < 0) {
 			player1.score++;
+			$player1Score.innerHTML = player1.score;
 			this.reset();
 		}
 		this.y += this.ySpeed;
@@ -89,13 +95,13 @@ function Paddle(x, y, width, height, speed) {
 }
 
 function Player() {
-	this.paddle = new Paddle(740, 200, 20, 100, 100);
-	this.score;
+	this.paddle = new Paddle(760, 200, 20, 100, 50);
+	this.score = 0;
 }
 
 function Computer() {
-	this.paddle = new Paddle(40, 200, 20, 100, 3);
-	this.score;
+	this.paddle = new Paddle(20, 200, 20, 100, 5);
+	this.score = 0;
 
 	this.update = function(ball) {
 		if (ball.y < this.paddle.y) {
